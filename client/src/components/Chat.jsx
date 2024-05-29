3958bf8fd11e2c6fe19072e2d1b7dac488194346
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-05-29 23:20:45 +0100

    scroll bottom

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import io from 'socket.io-client';
 import axios from 'axios';
 import './Chat.css'
 import { BsArrowLeft } from 'react-icons/bs';
+
 const Chat = () => {
     const { userId, profileId } = useParams();
+    const navigate = useNavigate();
     const [userDetails, setUserDetails] = useState({});
-const navigate= useNavigate()
     const [messages, setMessages] = useState([]);
     const [inputMessage, setInputMessage] = useState('');
     const [socket, setSocket] = useState(null);
+    const chatMessagesRef = useRef(null); // Ref for chat messages container
 
     useEffect(() => {
         // Connect to Socket.IO server only once when the component mounts
@@ -26,25 +28,31 @@ const navigate= useNavigate()
             try {
                 const response = await axios.get(`/api/messages/getMessages/${userId}/${profileId}`);
                 setMessages(response.data);
+                // Scroll to bottom after setting messages
+                scrollToBottom();
             } catch (error) {
                 console.error('Error fetching messages:', error);
             }
         };
-         // Fetch user details
-    const fetchUserDetails = async () => {
-        try {
-            const response = await axios.get(`/api/users/findbyId/${profileId}`);
-            setUserDetails(response.data);
-        } catch (error) {
-            console.error('Error fetching user details:', error);
-        }
-    };
-    fetchUserDetails();
+
+        // Fetch user details
+        const fetchUserDetails = async () => {
+            try {
+                const response = await axios.get(`/api/users/findbyId/${profileId}`);
+                setUserDetails(response.data);
+            } catch (error) {
+                console.error('Error fetching user details:', error);
+            }
+        };
+
         fetchMessages();
+        fetchUserDetails();
 
         // Listen for received messages
         newSocket.on('receiveMessage', (message) => {
             setMessages(prevMessages => [...prevMessages, message]);
+            // Scroll to bottom after receiving a new message
+            scrollToBottom();
         });
 
         // Clean up function to disconnect socket when component unmounts
@@ -59,41 +67,47 @@ const navigate= useNavigate()
         const roomId = [userId, profileId].sort().join('-');
 
         // Emit message to Socket.IO server
-        socket.emit('sendMessage', { roomId, senderId: userId,receiverId:profileId, content: inputMessage });
+        socket.emit('sendMessage', { roomId, senderId: userId, receiverId: profileId, content: inputMessage });
 
         // Clear input field
         setInputMessage('');
     };
-   
+
+    // Function to scroll chat messages container to bottom
+    const scrollToBottom = () => {
+        if (chatMessagesRef.current) {
+            chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
+        }
+    };
 
     return (
         <div className="chat-card">
-        <div className="chat-header">
-                <button className="back-button" onClick={()=>{navigate('/messages/${userId}')}}>
+            <div className="chat-header">
+                <button className="back-button" onClick={() => { navigate(`/messages/${userId}`) }}>
                     <BsArrowLeft size={24} />
                 </button>
                 <img src={userDetails.profilePicture} alt={userDetails.username} className="chat-header-img" />
                 <h2 className="chat-header-username">{userDetails.username}</h2>
             </div>
-        <div className="chat-body">
-            <div className="chat-messages">
-                {messages.map((message, index) => (
-                    <div key={index} className={message.senderId === userId ? 'chat-message chat-message-sent' : 'chat-message chat-message-received'}>
-                        {message.content}
-                    </div>
-                ))}
-            </div>
-            <div className="chat-input-container">
-                <input 
-                    type="text" 
-                    value={inputMessage} 
-                    onChange={(e) => setInputMessage(e.target.value)} 
-                    className="chat-input"
-                />
-                <button onClick={handleSendMessage} className="chat-send-button">Send</button>
+            <div className="chat-body" ref={chatMessagesRef}>
+                <div className="chat-messages">
+                    {messages.map((message, index) => (
+                        <div key={index} className={message.senderId === userId ? 'chat-message chat-message-sent' : 'chat-message chat-message-received'}>
+                            {message.content}
+                        </div>
+                    ))}
+                </div>
+                <div className="chat-input-container">
+                    <input
+                        type="text"
+                        value={inputMessage}
+                        onChange={(e) => setInputMessage(e.target.value)}
+                        className="chat-input"
+                    />
+                    <button onClick={handleSendMessage} className="chat-send-button">Send</button>
+                </div>
             </div>
         </div>
-    </div>
     );
 };
 
